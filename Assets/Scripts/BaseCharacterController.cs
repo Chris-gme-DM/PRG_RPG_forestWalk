e170956a9848e41e13af99d2e0383cbb8ab87688
Christof Kloninger <kloningerchristof@gmail.com>
2025-04-16 14:05:25 +0200

    ADD Tilemap ADD Collision FIX Controls

--- Assets/Scripts/BaseCharacterController.cs
+++ Assets/Scripts/BaseCharacterController.cs
@@ -6,13 +6,41 @@ using static UnityEngine.InputSystem.InputAction;
 public class BaseCharacterController : MonoBehaviour
 {
     private Vector2 movementInput;
-    [SerializeField] float movementSpeed;
+    [Range(0.5f, 4f)][SerializeField] private float movementSpeed;
+    [Range(0, 10f)][SerializeField] float movementAccelaration;
+    [Range(0,1f)][SerializeField] private float slowFactor;
+    private Rigidbody2D rb;
+    private bool isSlowed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isSlowed = false;
+    }
     public void Movement(CallbackContext ctx) 
-     {
+    {
         movementInput = ctx.ReadValue<Vector2>();
-     }
-    private void Update()
+    }
+    private void FixedUpdate()
     {  
-        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed;
+        var actualMovementSpeed = isSlowed ? movementSpeed * slowFactor : movementSpeed;
+        // Transform to move the player
+        transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * actualMovementSpeed);
+        // Will add Force because Physics to smooth out movement
+        rb.AddForce(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * actualMovementSpeed * movementAccelaration, ForceMode2D.Force);
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Swamp"))
+        {
+            isSlowed = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Swamp"))
+        {
+            isSlowed = false;
+        }
     }
 }
